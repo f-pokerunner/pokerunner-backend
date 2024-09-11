61da71a7bf60e8755ffa503b67eca5ac2c5f1f94
Soeun Uhm <uhmturks@yonsei.ac.kr>
2024-09-11 18:35:06 +0900

    fix : 중복 파일 삭제

--- src/main/java/com/flab/pokerunner/repository/UserPokemonRepository.java
+++
@@ -1,7 +0,0 @@
-package com.flab.pokerunner.repository;
-
-import com.flab.pokerunner.domain.entity.UserPokemon;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserPokemonRepository extends JpaRepository<UserPokemon, Long> {
-}
