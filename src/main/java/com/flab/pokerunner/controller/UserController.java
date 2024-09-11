91135381cb7ccf8f6c8eb5f43159450ef10b9382
Soeun Uhm <uhmturks@yonsei.ac.kr>
2024-09-11 20:32:42 +0900

    feat : 닉네임 중복 확인

--- src/main/java/com/flab/pokerunner/controller/UserController.java
+++ src/main/java/com/flab/pokerunner/controller/UserController.java
@@ -1,12 +1,14 @@
 package com.flab.pokerunner.controller;
 
-import com.flab.pokerunner.domain.dto.UserLoginRequestDTO;
+import com.flab.pokerunner.domain.dto.UserSignUpRequestDTO;
 import com.flab.pokerunner.domain.dto.UuidRequestDTO;
-import com.flab.pokerunner.service.UserService;
+import com.flab.pokerunner.service.user.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,15 +28,26 @@ public class UserController {
         return ResponseEntity.ok(isExistingUser);
     }
 
-    @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody UserLoginRequestDTO userLoginRequestDTO) {
-        boolean isnNewUser = userService.login(userLoginRequestDTO);
+    @GetMapping("/checkUserNicknameExist/{user_nickname}")
+    public ResponseEntity<Boolean> checkUserNicknameExists(@PathVariable String user_nickname) {
+        boolean userNicknameExists = userService.checkUserNicknameExists(user_nickname);
+        return ResponseEntity.ok(userNicknameExists);
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<String> signup(@RequestBody UserSignUpRequestDTO userSignUpRequestDTO) {
+        boolean isnNewUser = userService.signup(userSignUpRequestDTO);
+        userService.saveUserPokemon(userSignUpRequestDTO);
         if (isnNewUser) {
-            return new ResponseEntity<>("User created successfully", HttpStatus.CREATED);
+            return new ResponseEntity<>("UserJpo created successfully", HttpStatus.CREATED);
         } else {
-            return new ResponseEntity<>("User logged in.", HttpStatus.OK);
+            return new ResponseEntity<>("UserJpo logged in.", HttpStatus.OK);
         }
     }
+
+
+
+
 
 
 
