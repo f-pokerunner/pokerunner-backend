e291fd537df3643408403f9707ee941d6e1bf735
김세준 <dailyzett@gmail.com>
2024-09-12 20:17:41 +0900

    fix: 엔드포인트 이름 변경

--- src/main/java/com/flab/pokerunner/controller/EvolutionSpotController.java
+++ src/main/java/com/flab/pokerunner/controller/EvolutionSpotController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/evolution-spots")
+@RequestMapping("/pokemon-spots")
 @RequiredArgsConstructor
 public class EvolutionSpotController {
 
