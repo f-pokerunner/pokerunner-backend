734a2b49f76de7742cee70cf40fa6665b7583c64
Soeun Uhm <uhmturks@yonsei.ac.kr>
2024-09-12 20:27:24 +0900

    fix : 수정

--- src/main/java/com/flab/pokerunner/domain/dto/UserRunningInfoDto.java
+++ src/main/java/com/flab/pokerunner/domain/dto/UserRunningInfoDto.java
@@ -1,9 +1,13 @@
 package com.flab.pokerunner.domain.dto;
 
 import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 
 @Builder
+@Getter
+@AllArgsConstructor
 public class UserRunningInfoDto {
     String distanceMeter;
     String pace;
